f59b74d54edda71f24290c47c9f3bd1607483579
Jessica Hernandez <jesherart@fullsail.edu>
2015-05-07 01:24:45 -0400

    Final upload of wk1 project

--- js/main.js
+++ js/main.js
@@ -9,68 +9,25 @@
     
 */
 
-
-/*(function ($) {
-    
-    $(".listIcon").hover( function() {
-            $(this).attr("src","images/checked.png");
-        }, 
-        function() {
-            $(".listIcon").attr("src","images/unchecked.png");
-        }
-    );
-  
-    }
-    
-    
-})(jQuery);*/ // end private scope
-
-/*
-$.noConflict();
-jQuery( document ).ready(function( $ ) {
-  
-    $(".listIcon").hover( function() {
-            $(this).attr("src","images/checked.png");
-        }, 
-        function() {
-            $(".listIcon").attr("src","images/unchecked.png");
-        }
-    );
-  
-    } 
-    
-});*/
-
-
-jQuery(document).ready(function($){
-    
-  $(".listIcon").hover(function() {
-    
-    $scope.$apply(function() {
-       $(this).attr("src","images/checked.png");
-    }),
-    $scope.$apply(function() {
-       $(".listIcon").attr("src","images/unchecked.png");
-    });  
-      
-  });
-    
-});
-
-
-
+/*MAIN APP CALL*/
 var app = angular.module('myApp', []);
 
+/*SPECIFYING THE APP CONTROLLER*/
 app.controller('GroceryController', function($scope){
 
     $scope.itemName;
 
+    
+    /*DEFAULT ITEMS LIST*/
     $scope.itemsArray = ['Bananas', 'Milk', 'Eggos', 'Mac & Cheese'];
 
+    
+    /*ADD ITEMS FUNCTION*/
     $scope.addItem = function() {
         
         var newItem = $scope.itemName;
         
+        /*VALIDATE EMPTY STRING OR UNDEFINED INPUT*/
         if ( newItem === '' || newItem === undefined ) {
               
             $scope.msg = "✖ Error: Cannot add a blank item. Try again.";    
@@ -79,10 +36,12 @@ app.controller('GroceryController', function($scope){
             
             var notOnList = true;
          
+            /*USE FOR LOOP TO ITTERATE THROUGH THE ITEMS LIST ARRAY*/
             for ( var i = 0; i <= $scope.itemsArray.length - 1; i++) {
                 
                 var currentListItem = $scope.itemsArray[i];
-             
+                
+                /*VALIDATE IF ITEM IS ON THE LIST ALREADY*/
                 if ( newItem === currentListItem ) {
                     
                     $scope.msg = "✖ Error: " + newItem + " is already in your grocery list.";
@@ -92,8 +51,9 @@ app.controller('GroceryController', function($scope){
                     
                 }
                 
-            }
+            } /*END OF FOR LOOP*/
             
+            /*WHEN ITEM IS NOT ON THE LIST, IT WILL BE ADDEDD TO THE ARRAY*/
             if ( notOnList == true ) {
                 
                 $scope.itemsArray.push($scope.itemName);
@@ -102,10 +62,12 @@ app.controller('GroceryController', function($scope){
                 
             }
             
-        }
+        } /*END OF ELSE STATEMENT*/
 
-    }
+    } /*END OF ADD ITEMS FUNCTION*/
 
+    
+    /*DELETE ITEMS FUNCTION*/
     $scope.deleteItem = function(itemToDelete) {
         var index = $scope.itemsArray.indexOf(itemToDelete);
 
@@ -113,3 +75,4 @@ app.controller('GroceryController', function($scope){
     }
     
 });
+/*END OF APP CONTROLLER*/
