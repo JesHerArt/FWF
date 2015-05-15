0a23d244ee52ca421f6ef6321af1d17fd6e8e8bd
Jessica Hernandez <jesherart@fullsail.edu>
2015-05-15 16:19:01 -0400

    updating first half of angular code working
    default employees load, add employee function and delete employee
    functions work.
    local storage data service js file still in progress

--- Wk2/js/app.js
+++ Wk2/js/app.js
@@ -15,7 +15,97 @@ var app = angular.module('myApp', []);
 /*SPECIFYING THE APP CONTROLLER*/
 app.controller('EmployeeController', function($scope){
 
+    $scope.empName;
+    $scope.empPosition;
+    $scope.empStAddress;
+    $scope.empCity;
+    $scope.empState;
+    $scope.empZip;
+    $scope.empPhone;
+    $scope.empEmail;
     
+    
+    /*DEFAULT EMPLOYEES LIST*/
+    $scope.employeesArray = [
+            {
+                name: "Garry Jones",
+                position: "Full Sail President",
+                phone: "1 (800) 226-7625",
+                email: "garry@fullsail.com",
+                address: "3300 University Boulevard",
+                city: "Winter Park",
+                state: "FL",
+                zip: "32792"
+            },
+            {
+                name: "Crystal Silvestro",
+                position: "Professor at Full Sail University",
+                phone: "(407) 679-0100",
+                email: "csilvestro@fullsail.com",
+                address: "3300 University Boulevard",
+                city: "Winter Park",
+                state: "FL",
+                zip: "32792"
+            },
+            {
+                name: "Jessica Hernandez",
+                position: "Student",
+                phone: "(786) 567-7223",
+                email: "jesherart@fullsail.edu",
+                address: "12345 South Road",
+                city: "Miami",
+                state: "FL",
+                zip: "33133"
+            },
+            {
+                name: "Asuka Kazama",
+                position: "Professional Fighter",
+                phone: "(407) 679-0100",
+                email: "asuka@tekken.com",
+                address: "4345 Sushi Park Way",
+                city: "Tokyo",
+                state: "JP",
+                zip: ""
+            }
+        ]
+    
+    
+    
+    /*ADD EMPLOYEE FUNCTION*/
+    $scope.addEmployee = function() {
+        
+        var newEmployee = {
+            name: $scope.empName,
+            position: $scope.empPosition,
+            phone: $scope.empPhone,
+            email: $scope.empEmail,
+            addy: $scope.empStAddress,
+            city: $scope.empCity,
+            state: $scope.empState,
+            zip: $scope.empZip 
+        };
+        
+        $scope.employeesArray.push(newEmployee);
+        
+        $scope.empName = '';
+        $scope.empPosition = '';
+        $scope.empStAddress = '';
+        $scope.empCity = '';
+        $scope.empState = '';
+        $scope.empZip = '';
+        $scope.empPhone = '';
+        $scope.empEmail = '';
+            
+    }
+
+    
+    
+    /*DELETE EMPLOYEE FUNCTION*/
+    $scope.deleteEmployee = function(empToDelete) {
+        var index = $scope.employeesArray.indexOf(empToDelete);
+
+        $scope.employeesArray.splice(index, 1);
+    }
     
 });
 /*END OF APP CONTROLLER*/
