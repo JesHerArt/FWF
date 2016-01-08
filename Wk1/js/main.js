03c050c6d12cfe9933f0eaf03e50a7f0f2a4d799
Jessica Hernandez <jesherart@fullsail.edu>
2016-01-07 23:11:46 -0500

    wk1

--- Wk1/js/main.js
+++ Wk1/js/main.js
@@ -13,27 +13,65 @@
 var app = angular.module('myApp', []);
 
 /*SPECIFYING THE APP CONTROLLER*/
-app.controller('GroceryController', function($scope, dataService){
+app.controller('GroceryController', function($scope){
 
     $scope.itemName;
 
     
     /*DEFAULT ITEMS LIST*/
-    $scope.itemsArray = dataService.getItems();
+    $scope.itemsArray = ['Bananas', 'Milk', 'Eggos', 'Mac & Cheese'];
 
     
     /*ADD ITEMS FUNCTION*/
     $scope.addItem = function() {
-        dataService.add($scope.itemName);
+        
+        var newItem = $scope.itemName;
+        
+        /*VALIDATE EMPTY STRING OR UNDEFINED INPUT*/
+        if ( newItem === '' || newItem === undefined ) {
+              
+            $scope.msg = "✖ Error: Cannot add a blank item. Try again.";    
+ 
+        } else {
             
-        $scope.itemName = '';
+            var notOnList = true;
+         
+            /*USE FOR LOOP TO ITTERATE THROUGH THE ITEMS LIST ARRAY*/
+            for ( var i = 0; i <= $scope.itemsArray.length - 1; i++) {
+                
+                var currentListItem = $scope.itemsArray[i];
+                
+                /*VALIDATE IF ITEM IS ON THE LIST ALREADY*/
+                if ( newItem === currentListItem ) {
+                    
+                    $scope.msg = "✖ Error: " + newItem + " is already in your grocery list.";
+                    $scope.itemName = '';
+                    notOnList = false;
+                    break;
+                    
+                }
+                
+            } /*END OF FOR LOOP*/
+            
+            /*WHEN ITEM IS NOT ON THE LIST, IT WILL BE ADDEDD TO THE ARRAY*/
+            if ( notOnList == true ) {
+                
+                $scope.itemsArray.push($scope.itemName);
+                $scope.itemName = '';
+                $scope.msg = "✔ Item Added";
+                
+            }
+            
+        } /*END OF ELSE STATEMENT*/
 
     } /*END OF ADD ITEMS FUNCTION*/
 
     
     /*DELETE ITEMS FUNCTION*/
     $scope.deleteItem = function(itemToDelete) {
-        dataService.removeItem(itemToDelete);
+        var index = $scope.itemsArray.indexOf(itemToDelete);
+
+        $scope.itemsArray.splice(index, 1);
     }
     
 });
